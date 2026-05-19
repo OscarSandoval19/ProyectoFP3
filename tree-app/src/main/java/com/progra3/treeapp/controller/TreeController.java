2bf14eef08ffc24e2109a0212f38b4b99ff1c7a9
Dilena Irene Grijalva Tenas <dilena13grijalva@gmail.com>
2026-05-19 10:39:17 -0600

    feat(controller): implementar endpoints de creacion de raiz e insercion de nodos

--- tree-app/src/main/java/com/progra3/treeapp/controller/TreeController.java
+++ tree-app/src/main/java/com/progra3/treeapp/controller/TreeController.java
@@ -9,7 +9,24 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@RequestMapping
 public class TreeController {
+	private final TreeAlgorithmStrategy<NodeDTO> treeStrategy;
 
+    public TreeController(TreeAlgorithmStrategy<NodeDTO> treeStrategy) {
+        this.treeStrategy = treeStrategy;
+    }
+    
+    @PostMapping("/nodes/root")
+    public ResponseEntity<NodeDTO> createRoot(@RequestBody NodeDTO data) {
+        treeStrategy.createRoot(data);
+        return ResponseEntity.status(HttpStatus.CREATED).body(data);
+    }
+    
+    @PostMapping("/nodes")
+    public ResponseEntity<NodeDTO> insertNode(@RequestParam String parentId, @RequestBody NodeDTO data) {
+        treeStrategy.insert(parentId, data);
+        return ResponseEntity.status(HttpStatus.CREATED).body(data);
+    }
 
 }
